44534f1eb82704f02cf62a56d62a981ba13462c7
shyamsha <shyamkumar97@live.com>
2023-05-31 11:13:05 +0530

    add

--- secondLargestInArray.js
+++ secondLargestInArray.js
@@ -1,7 +1,7 @@
 // Find Second largest element in an array
 function findSecondLargestElem(arr) {
-  let first = -1,
-    second = -1;
+  let first = 0,
+    second = 0;
 
   for (let i = 0; i <= arr.length - 1; i++) {
     if (arr[i] > first) {
@@ -14,4 +14,23 @@ function findSecondLargestElem(arr) {
   console.log(second, first);
 }
 let arr = [12, 35, 1, 10, 34, 1];
+
 findSecondLargestElem(arr);
+
+let arr1 = [-12, -35, -1, -10, -34];
+
+function findNegativeSecondLargestElem(arr) {
+  let first = -Infinity,
+    second = -Infinity;
+
+  for (let i = 0; i <= arr.length - 1; i++) {
+    if (arr[i] > first) {
+      second = first;
+      first = arr[i];
+    } else if (arr[i] > second && arr[i] != first) {
+      second = arr[i];
+    }
+  }
+  console.log(second, first);
+}
+console.log(findNegativeSecondLargestElem(arr1));
