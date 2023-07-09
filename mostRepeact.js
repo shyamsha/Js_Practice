cb07b3767da5329e9026f2db0e28344a365393e6
shyamsha <shyamkumar97@live.com>
2023-07-09 13:01:09 +0530

    add

---
+++ mostRepeact.js
@@ -0,0 +1,22 @@
+const { log } = require("console");
+
+// const arr =[1,3,4,3,4,5,5,2,3,6,7,8,2]
+const arr = ["a", "a", "c", "d", "c", "c", "c", "a", "a", "c", "d"];
+
+let mostRepeat = function (array) {
+  let obj = {};
+  let maxEl = array[0],
+    maxCount = 1;
+  for (let i = 0; i < array.length; i++) {
+    let el = array[i];
+    if (!obj[el]) obj[el] = 1;
+    else obj[el]++;
+    if (obj[el] > maxCount) {
+      maxEl = el;
+      maxCount = obj[el];
+    }
+  }
+  return maxEl;
+};
+
+log(mostRepeat(arr));
