0b766ecaa8c2a2aa509e86c44ce60cd719345c52
shyamsha <shyamkumar97@live.com>
2023-08-05 13:29:15 +0530

    add

--- longestOnes.js
+++ longestOnes.js
@@ -54,4 +54,51 @@ var longestOnes1 = function (nums, k) {
 };
 let nums = [0, 1, 1, 1, 0, 0, 0, 1, 1, 1, 1, 0];
 let k = 2;
-console.log(longestOnes(nums, k));
+console.log(longestOnes1(nums, k));
+
+// Longest Subarray of 1's After Deleting One Element
+
+var longestSubarray = function (nums) {
+  let count = 0;
+  let longest = 0;
+  let start = 0;
+  let flip = 1;
+  for (let i = 0; i < nums.length; i++) {
+    let item = nums[i];
+    if (item === 1) {
+      count++;
+    } else if (item === 0 && flip !== 0) {
+      count++;
+      flip--;
+    } else {
+      longest = Math.max(longest, count);
+      i = start;
+      start++;
+      count = 0;
+      flip = 1;
+    }
+  }
+  longest = Math.max(count, longest) - 1;
+  if (longest > 0) {
+    return longest;
+  }
+  return 0;
+};
+
+var longestSubarray1 = function (nums) {
+  let zeroCount = 0;
+  let longestWindow = 0;
+  let start = 0;
+  for (let i = 0; i < nums.length; i++) {
+    zeroCount += nums[i] === 0 ? 1 : 0;
+    while (zeroCount > 1) {
+      zeroCount -= nums[start] === 0 ? 1 : 0;
+      start++;
+    }
+    longestWindow = Math.max(longestWindow, i - start);
+  }
+  return longestWindow;
+};
+
+let arr = [0, 1, 1, 1, 0, 1, 1, 0, 1]; //5
+console.log(longestSubarray(arr));
