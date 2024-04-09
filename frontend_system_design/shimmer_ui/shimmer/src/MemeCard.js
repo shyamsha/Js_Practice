87d5a4fb8df74172f2295278991ea40824998adf
shyamsha <shyamkumar97@live.com>
2024-04-09 16:42:36 +0530

    remove border

--- frontend_system_design/shimmer_ui/shimmer/src/MemeCard.js
+++ frontend_system_design/shimmer_ui/shimmer/src/MemeCard.js
@@ -2,7 +2,7 @@ import React from "react";
 
 function MemeCard({ item }) {
   return (
-    <div className="m-5 border-2 border-black rounded-lg">
+    <div className="m-5 border-2 rounded-lg">
       <img className="w-48 h-48" src={item.url} alt={item.title} />
       <p>{item.author}</p>
     </div>
--- frontend_system_design/shimmer_ui/shimmer/src/Shimmer.js
+++ frontend_system_design/shimmer_ui/shimmer/src/Shimmer.js
@@ -4,7 +4,7 @@ function Shimmer() {
   return Array(24)
     .fill(0)
     .map((_, i) => (
-      <div key={i} className="m-5 border-2 border-black rounded-lg">
+      <div key={i} className="m-5 border-2 rounded-lg">
         <div className="w-48 h-48 bg-gray-200 animate-pulse" />
       </div>
     ));
