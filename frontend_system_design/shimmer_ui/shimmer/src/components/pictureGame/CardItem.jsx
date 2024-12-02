6e60e99ce9cd6b1844f5ee191c132a74d6b3315f
shyamsha <shyamkumar97@live.com>
2024-12-02 19:44:53 +0530

    remove logs

--- frontend_system_design/shimmer_ui/shimmer/src/components/pictureGame/CardItem.jsx
+++ frontend_system_design/shimmer_ui/shimmer/src/components/pictureGame/CardItem.jsx
@@ -2,7 +2,6 @@ import { blankCard as defaultImage } from "./images";
 
 export const CardItem = ({ isOpen, image, item, onClick }) => {
   const imagePath = isOpen ? image : defaultImage;
-  // console.log(image);
   return (
     <div className="card-item" onClick={() => onClick(item)}>
       <img src={imagePath} alt="Image" />
