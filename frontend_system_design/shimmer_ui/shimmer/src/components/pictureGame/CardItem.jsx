3b1b93d23a020d13a72ee37fef6851e2a3bbad66
shyamsha <shyamkumar97@live.com>
2024-12-02 19:55:44 +0530

    fix alt prop in img tag

--- frontend_system_design/shimmer_ui/shimmer/src/components/pictureGame/CardItem.jsx
+++ frontend_system_design/shimmer_ui/shimmer/src/components/pictureGame/CardItem.jsx
@@ -4,7 +4,7 @@ export const CardItem = ({ isOpen, image, item, onClick }) => {
   const imagePath = isOpen ? image : defaultImage;
   return (
     <div className="card-item" onClick={() => onClick(item)}>
-      <img src={imagePath} alt="Image" />
+      <img src={imagePath} alt="ImageURL" />
     </div>
   );
 };
