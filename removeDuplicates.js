1f50b2120d44841643496ab0165029923d19ecda
shyamsha <shyamkumar97@live.com>
2025-01-17 21:00:04 +0530

    add Q

--- removeDuplicates.js
+++ removeDuplicates.js
@@ -18,7 +18,7 @@ var removeDuplicates = function (nums) {
   console.log(nums);
   return count + 1;
 };
-let nums = [0, 0, 1, 1, 1, 2, 2, 3, 3, 4];
+// let nums = [0, 0, 1, 1, 1, 2, 2, 3, 3, 4];
 
 // console.log(removeDuplicates(nums));
 
@@ -32,4 +32,36 @@ let removeDuplicates1 = function (nums) {
   }
   return j;
 };
-console.log(removeDuplicates1(nums));
+// console.log(removeDuplicates1(nums));
+
+// Remove Duplicates from Sorted Array II
+// Given a sorted array nums, remove the duplicates in-place such that duplicates appeared at most twice and return the new length.
+function removeDuplicates2(nums) {
+  // let count = 1;
+  // let j = 1;
+
+  // for (let i = 1; i < nums.length; i++) {
+  //   if (nums[i] === nums[i - 1]) {
+  //     count++;
+  //   } else {
+  //     count = 1;
+  //   }
+  //   if (count <= 2) {
+  //     nums[j] = nums[i];
+  //     j++;
+  //   }
+  // }
+  // console.log(nums);
+  // return j;
+  let j = 1;
+  for (let i = 1; i < nums.length; i++) {
+    if (nums[j - 2] !== nums[i]) {
+      nums[j] = nums[i];
+      j++;
+    }
+  }
+  return j;
+}
+// let nums = [1, 1, 1, 2, 2, 3];
+let nums = [0, 0, 1, 1, 1, 1, 2, 3, 3];
+console.log(removeDuplicates2(nums));
