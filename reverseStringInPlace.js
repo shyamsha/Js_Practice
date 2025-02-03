de79f9c60663baff92a658cc9a818ea4593f9f81
shyamsha <shyamkumar97@live.com>
2025-02-03 21:21:28 +0530

    add Q

--- reverseStringInPlace.js
+++ reverseStringInPlace.js
@@ -27,4 +27,30 @@ function reverseStringWithRecursion(str) {
     return reverseStringWithRecursion(str.substr(1)) + str[0];
   }
 }
-console.log(reverseStringWithRecursion("JavaScript is awesome"));
+// console.log(reverseStringWithRecursion("JavaScript is awesome"));
+
+// Input: s = "the sky is blue"
+// Output: "blue is sky the"
+
+// Input: s = "a good   example"
+// Output: "example good a"
+// Explanation: You need to reduce multiple spaces between two words to a single space in the reversed string
+
+let reverseWordsInPlace = function (s) {
+  // with out taking extra space
+  // s = s.split(" ").filter((val) => val !== "");
+  // let res = "";
+  // let j = s.length - 1;
+  // for (let i = 0; i < s.length; i++) {
+  //   let temp = s[i];
+  //   res += s[j] + " ";
+  //   s[j] = temp;
+
+  //   j--;
+  // }
+  // return res.trim();
+  return s.trim().split(/\s+/).reverse().join(" ");
+};
+let s = " a good   example";
+
+console.log(reverseWordsInPlace(s)); // Output: "example good a"
